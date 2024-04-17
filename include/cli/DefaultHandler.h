1280879e74426863544d92e0cc9a06ebc8fe9cc2
Lenina-Paket <artyom.kalini4enko2004@gmail.com>
2024-04-17 21:08:42 +0300

    def hadler model command done

--- include/cli/DefaultHandler.h
+++ include/cli/DefaultHandler.h
@@ -1,14 +1,35 @@
 #pragma once
 
 #include "cli/Handler.h"
+#include "base/Model.h"
+#include <unordered_map>
 
 namespace erconv {
-    
+
 class DefaultHandler : public IHandler {
 public:
+    DefaultHandler() : models(), current() {};
     void Execute(const Command& args, std::ostream& out) override;
 
 private:
+    std::unordered_map<std::string, ERModel> models;
+    std::pair<std::string, ERModel> current;
+
+private:
+    void ModelNewName(const std::string name);
+    void ModelDeleteName(const std::string name);
+    void ModelSelectName(const std::string name);
+    void ModelSelected();
+    void ModelList();
+    void ModelClear();
+
+    void ModelEntityNewName(const std::string name);
+    void ModelEntityDeleteName(const std::string name);
+    void ModelEntityList();
+    void ModelEntityAttributeAdd(const std::string   entityName, 
+                                const std::string   fieldName, 
+                                const DataTypeEntity &fieldType, 
+                                const std::vector<ConstraintsEntity> &fieldConstr);
 
 };
 
--- src/cli/DefaultHandler.cpp
+++ src/cli/DefaultHandler.cpp
@@ -5,10 +5,130 @@ namespace erconv {
 void DefaultHandler::Execute(const Command& args, std::ostream& out) {
     // Здесь вся обработка запросов.
     // Dummy code:
-    for (const auto& arg : args) {
-        out << arg << " ";
+    // for (const auto& arg : args) {
+    //     out << arg << " ";
+    // }
+    // out << std::endl;
+    if (args[0] == "model") {
+
+        if (args[1] == "new") {
+            ModelNewName(args[2]);
+        }
+
+        if (args[1] == "delete") {
+            ModelDeleteName(args[2]);
+        }
+
+        if (args[1] == "select") {
+            ModelSelectName(args[2]);
+        }
+
+        if (args[1] == "selected") {
+            ModelSelected();
+        }
+
+        if (args[1] == "list") {
+            ModelList();
+        }
+
+        if (args[1] == "clear") {
+            ModelClear();
+        }
     }
-    out << std::endl;
+
+    if (args[0] == "entity") {
+        if (args[1] == "new") {
+            ModelEntityNewName(args[2]);
+        }
+
+        if (args[1] == "delete") {
+            ModelEntityDeleteName(args[2]);
+        }
+
+        if (args[1] == "list") {
+            ModelEntityList();
+        }
+    }
 }
+
+void DefaultHandler::ModelNewName(const std::string name) {
+    if (models.find(name) != models.end()) {
+        std::cout << "Model with name " << name << " already exists!" << std::endl;
+    } else {
+        ERModel model;
+        models.insert({name, model});
+        current = {name, model};
+    }
+}
+
+void DefaultHandler::ModelDeleteName(const std::string name) {
+    models.erase(name);
+    if (current.first == name) {
+        current.first = models.begin()->first;
+        current.second = models.begin()->second;
+    }
+}
+
+void DefaultHandler::ModelSelectName(const std::string name) {
+    if (models.find(name) == models.end()) {
+        std::cout << "Model with name " << name << " not in list" << std::endl;
+    } else {
+        current.first = name;
+        current.second = models[name];
+    }
+}
+
+void DefaultHandler::ModelSelected() {
+    if (models.size() != 0) {
+        std::cout << current.first << std::endl;
+    } else {
+        std::cout << "There are no models" << std::endl;
+    }
+}
+
+void DefaultHandler::ModelList() {
+    if (models.size() != 0) {
+        std::cout << "There are models: " << std::endl;
+        for (const auto& mod : models) {
+            std::cout << mod.first << std::endl;
+        }
+    } else {
+        std::cout << "There are no models" << std::endl;
+    }
+}
+
+void DefaultHandler::ModelClear() {
+    models.clear();
+    std::cout << "All models are deleted" << std::endl;
+}
+
+void DefaultHandler::ModelEntityNewName(const std::string name) {
+    if (models.size() != 0) {
+        std::cout << "There are no models" << std::endl;
+    } else {
+        current.second.AddEntity(name);
+    }
+}
+
+void DefaultHandler::ModelEntityDeleteName(const std::string name) {
+    if (models.size() != 0) {
+        std::cout << "There are no models" << std::endl;
+    } else {
+        current.second.RemoveEntity(name);
+    }
+}
+
+void DefaultHandler::ModelEntityList() {
+    if (models.size() != 0) {
+        std::cout << "There are no models" << std::endl;
+    } else {
+        std::cout << "There are list of entities of model " << current.first << std::endl;
+        for (const auto ent : current.second.GetEntities()) {
+            std::cout << ent.GetName() << std::endl;
+        }
+    }
+}
+
+
 
 } // namespace erconv
