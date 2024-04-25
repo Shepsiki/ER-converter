c2b53f2eba33a2943f8b7ec8ea5ec0b39be6a2f0
Lenina-Paket <artyom.kalini4enko2004@gmail.com>
2024-04-25 23:26:17 +0300

    entity commands is done

--- include/cli/DefaultHandler.h
+++ include/cli/DefaultHandler.h
@@ -2,6 +2,7 @@
 
 #include "cli/Handler.h"
 #include "base/Model.h"
+#include "base/Entity.h"
 #include <unordered_map>
 
 namespace erconv {
@@ -16,20 +17,27 @@ private:
     std::pair<std::string, ERModel> current;
 
 private:
-    void ModelNewName(const std::string name);
-    void ModelDeleteName(const std::string name);
-    void ModelSelectName(const std::string name);
+    int FromStringToDataType(const std::string& str);
+    int FromStringToCovType(const std::string str);
+
+
+    void ModelNewName(const std::string& name);
+    void ModelDeleteName(const std::string& name);
+    void ModelSelectName(const std::string& name);
     void ModelSelected();
     void ModelList();
     void ModelClear();
 
-    void ModelEntityNewName(const std::string name);
-    void ModelEntityDeleteName(const std::string name);
+    void ModelEntityNewName(const std::string& name);
+    void ModelEntityDeleteName(const std::string& name);
     void ModelEntityList();
-    void ModelEntityAttributeAdd(const std::string   entityName, 
-                                const std::string   fieldName, 
+    void ModelEntityAttributeAdd(const std::string&   entityName, 
+                                const std::string&   fieldName, 
                                 const DataTypeEntity &fieldType, 
                                 const std::vector<ConstraintsEntity> &fieldConstr);
+    void ModelEntityAttributeDelete(const std::string& name, const std::string& field);
+    void ModelEntityAttributeList(const std::string& name);
+    void ModelEntityAttributeClear(const std::string& name);
 
 };
 
--- src/cli/DefaultHandler.cpp
+++ src/cli/DefaultHandler.cpp
@@ -9,6 +9,9 @@ void DefaultHandler::Execute(const Command& args, std::ostream& out) {
     //     out << arg << " ";
     // }
     // out << std::endl;
+    
+
+    // MODEL COMMANDS BEGIN // 
     if (args[0] == "model") {
 
         if (args[1] == "new") {
@@ -35,7 +38,9 @@ void DefaultHandler::Execute(const Command& args, std::ostream& out) {
             ModelClear();
         }
     }
+    // MODEL COMMANDS END //
 
+    // ENTITY COMMANDS BEGIN //
     if (args[0] == "entity") {
         if (args[1] == "new") {
             ModelEntityNewName(args[2]);
@@ -48,10 +53,84 @@ void DefaultHandler::Execute(const Command& args, std::ostream& out) {
         if (args[1] == "list") {
             ModelEntityList();
         }
+
+        if (args[1] == "attribute") {
+            if (args[2] == "add") {
+                DataTypeEntity enumType = DataTypeEntity(FromStringToDataType(args[5]));
+                std::vector<ConstraintsEntity> fieldConstr;
+                for (int i = 6; i < args.size(); i++) {
+                    ConstraintsEntity fieldType = ConstraintsEntity(FromStringToCovType(args[i]));
+                    fieldConstr.push_back(fieldType);
+                }
+                ModelEntityAttributeAdd(args[3], args[4], enumType, fieldConstr);
+            }
+
+            if (args[2] == "delete") {
+                ModelEntityAttributeDelete(args[3], args[4]);
+            }
+
+            if (args[2] == "list") {
+               ModelEntityAttributeList(args[3]);
+            }
+
+            if (args[2] == "clear") {
+                void ModelEntityAttributeClear();
+            }
+        
+        }
+    // ENTITY COMMANDS END //
     }
 }
 
-void DefaultHandler::ModelNewName(const std::string name) {
+int DefaultHandler::FromStringToDataType(const std::string& str) {
+    if (str == "SMALL_INT_T")
+        return 1;
+    if (str == "INT_T")
+        return 2;
+    if (str == "BIG_INT_T")
+        return 3;
+    if (str == "NUMERIC_DEC_T")
+        return 4;
+    if (str == "REAL_T")
+        return 5;
+    if (str == "DOUBLE_PRECIS_T")
+        return 6;
+    if (str == "FLOAT_T")
+        return 7;
+    if (str == "CHAR_T")
+        return 8;
+    if (str == "VARCHAR_T")
+        return 9;
+    if (str == "TEXT_T")
+        return 10;
+    if (str == "DATE_T")
+        return 11;
+    if (str == "TIME_T")
+        return 12;
+    if (str == "TIMESTAMP_WITH_TZ_T")
+        return 13;
+    if (str == "TIMESTAMP_WITHOUT_TZ_T")
+        return 14;
+    return 0;
+}
+
+int DefaultHandler::FromStringToCovType(const std::string str) {
+    if (str == "NOT_NULL_C")
+        return 1;
+    if (str == "NULL_C")
+        return 2;
+    if (str == "UNIQUE_C")
+        return 3;
+    if (str == "PRIMARY_KEY_C")
+        return 4;
+    if (str == "FOREIGN_KEY_C")
+        return 5;
+    if (str == "DEFAULT_C")
+        return 6;
+    return 0;
+}
+
+void DefaultHandler::ModelNewName(const std::string& name) {
     if (models.find(name) != models.end()) {
         std::cout << "Model with name " << name << " already exists!" << std::endl;
     } else {
@@ -61,7 +140,7 @@ void DefaultHandler::ModelNewName(const std::string name) {
     }
 }
 
-void DefaultHandler::ModelDeleteName(const std::string name) {
+void DefaultHandler::ModelDeleteName(const std::string& name) {
     models.erase(name);
     if (current.first == name) {
         current.first = models.begin()->first;
@@ -69,7 +148,7 @@ void DefaultHandler::ModelDeleteName(const std::string name) {
     }
 }
 
-void DefaultHandler::ModelSelectName(const std::string name) {
+void DefaultHandler::ModelSelectName(const std::string& name) {
     if (models.find(name) == models.end()) {
         std::cout << "Model with name " << name << " not in list" << std::endl;
     } else {
@@ -102,33 +181,73 @@ void DefaultHandler::ModelClear() {
     std::cout << "All models are deleted" << std::endl;
 }
 
-void DefaultHandler::ModelEntityNewName(const std::string name) {
+void DefaultHandler::ModelEntityNewName(const std::string& name) {
     if (models.size() != 0) {
-        std::cout << "There are no models" << std::endl;
-    } else {
         current.second.AddEntity(name);
+    } else {
+        std::cout << "There are no models" << std::endl;
     }
 }
 
-void DefaultHandler::ModelEntityDeleteName(const std::string name) {
+void DefaultHandler::ModelEntityDeleteName(const std::string& name) {
     if (models.size() != 0) {
-        std::cout << "There are no models" << std::endl;
-    } else {
         current.second.RemoveEntity(name);
+    } else {
+        std::cout << "There are no models" << std::endl;
     }
 }
 
 void DefaultHandler::ModelEntityList() {
     if (models.size() != 0) {
-        std::cout << "There are no models" << std::endl;
-    } else {
         std::cout << "There are list of entities of model " << current.first << std::endl;
         for (const auto ent : current.second.GetEntities()) {
             std::cout << ent.GetName() << std::endl;
         }
+    } else {
+        std::cout << "There are no models" << std::endl;
     }
 }
 
+void DefaultHandler::ModelEntityAttributeAdd(const std::string&   entityName, 
+                                            const std::string&   fieldName, 
+                                            const DataTypeEntity &fieldType, 
+                                            const std::vector<ConstraintsEntity> &fieldConstr) {
+    if (models.size() != 0)
+        current.second.AddEntityField(entityName, fieldName, fieldType, fieldConstr);
+    else
+        std::cout << "There are no models" << std::endl;
+}
+
+void DefaultHandler::ModelEntityAttributeDelete(const std::string& name, const std::string& field) {
+    if (models.size() != 0)
+        current.second.RemoveEntityField(name, field);
+    else
+        std::cout << "There are no models" << std::endl;
+}
+
+void DefaultHandler::ModelEntityAttributeList(const std::string& name) {
+    if (models.size() != 0) {
+        Entity ent = current.second.GetEntity(name);
+        const std::vector<TEntityField> vec = ent.GetAllFields();
+        if (vec.size() != 0)
+            std::cout << "Attributes of entity " << name << ":\n";
+        for (int i = 0; i < vec.size(); i++) {
+            std::cout << vec[i].Name << std::endl;
+        }
+    }
+    else
+        std::cout << "There are no models" << std::endl;
+}
+
+void DefaultHandler::ModelEntityAttributeClear(const std::string& name) {
+    if (models.size() != 0) {
+        // Entity ent = current.second.GetEntity(name);
+        // Не хватает метода Clear или DeleteAllFields для класса Entity
+        // Есть только удаление по имени сущности DeleteField(); 
+    } else {
+        std::cout << "There are no models" << std::endl;
+    }
+}
 
 
 } // namespace erconv
